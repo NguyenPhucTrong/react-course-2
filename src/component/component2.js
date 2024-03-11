7bb3b250a7a0cb9e36ece11a65eee95f94184a1f
NguyenPhucTrong <tronga7z6@gmail.com>
2024-03-11 19:27:00 +0700

    #39 Fragments in React

---
+++ src/component/component2.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Component2() {
+  return (
+    <React.Fragment>
+      <h3>This is my component2</h3>;
+    </React.Fragment>
+  );
+}
+
+export default Component2;
