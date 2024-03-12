8f348849492bee4156a5dc3288e31791606cd6be
NguyenPhucTrong <tronga7z6@gmail.com>
2024-03-12 10:53:09 +0700

    #42 Understanding side effects in React

--- src/App.js
+++ src/App.js
@@ -2,15 +2,40 @@
 import React from "react";
 import "./App.css";
 // import Component1 from "./component/component1";
-import RefsDemo from "./component/RefsDemo";
+// import RefsDemo from "./component/RefsDemo";
+import { useState, useEffect } from "react";
 
 function App() {
+  console.log("test");
+  let [resourseType, setResourseType] = useState("Home");
+  useEffect(() => {
+    console.log(resourseType);
+  }, [resourseType]);
   return (
-    <React.Fragment className="App">
-      {/* <h1>This is App Component</h1>
-      <Component1 /> */}
-      <RefsDemo />
-    </React.Fragment>
+    <div className="App">
+      <button
+        onClick={() => {
+          setResourseType("Home");
+        }}
+      >
+        Home
+      </button>
+      <button
+        onClick={() => {
+          setResourseType("About");
+        }}
+      >
+        About
+      </button>
+      <button
+        onClick={() => {
+          setResourseType("Contact");
+        }}
+      >
+        Contact
+      </button>
+      <h3>{resourseType}</h3>
+    </div>
   );
 }
 
