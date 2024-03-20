09eff05fbed4625500c5e9920c341e1064dbbce5
NguyenPhucTrong <tronga7z6@gmail.com>
2024-03-20 11:34:42 +0700

    #64 Managing States & Events

--- src/component/Demo.js
+++ src/component/Demo.js
@@ -1,10 +1,24 @@
 import { Component } from "react";
 class Demo extends Component {
+  constructor() {
+    super();
+    this.state = { showParagraph: true, count: 0 };
+  }
+
+  toggaleParagraph() {
+    // this.state.showParagraph = false;
+    console.log(this);
+    this.setState((currentStale) => {
+      return { showParagraph: !currentStale.showParagraph };
+    });
+  }
+
   render() {
     return (
       <>
         <h1>{this.props.name}</h1>
-        <p>{this.props.children}</p>
+        {this.state.showParagraph && <p>{this.props.children}</p>}
+        <button onClick={this.toggaleParagraph.bind(this)}>Show</button>
       </>
     );
   }
